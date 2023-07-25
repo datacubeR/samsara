60ba70558bdd18a5180b6614ce370beb4d319f91
datacubeR <alfonso.tobar@alumnos.usm.cl>
2023-07-25 11:53:56 -0400

    Adding new Viz to EDA homepage

--- EDA/EDA_home.py
+++ EDA/EDA_home.py
@@ -11,7 +11,20 @@ if "EXCLUDE_COLUMNS" not in st.session_state:
 if "df_dict" not in st.session_state:
     st.session_state.df_dict = import_data()
 
-col1, col2, col3, col4, col5 = st.columns(5)
+
+def create_histogram(event, color):
+    plot = (
+        st.session_state.df_dict[event]["not_null_perc"]
+        .plot(
+            kind="hist",
+            title="Histograma de % de Valores No Nulos",
+            color=color,
+            edgecolor="black",
+            bins=100,
+        )
+        .figure
+    )
+    return st.pyplot(plot, clear_figure=True)
 
 
 def summary_data(evento):
@@ -21,22 +34,18 @@ def summary_data(evento):
         label="Dimensiones", value=f"{st.session_state.df_dict[evento].shape}"
     )
     container.metric(
-        label="Porcentaje Medio de Mediciones",
-        value=f"{st.session_state.df_dict[evento]['not_null_perc'].mean():.2f}%",
+        label="Mediciones Promedio",
+        value=f"{st.session_state.df_dict[evento]['not_null'].mean():.0f}/{st.session_state.df_dict[evento]['not_null_perc'].mean():.0f}%",
+    )
+    container.metric(
+        label="Mínimo de Mediciones",
+        value=f"{st.session_state.df_dict[evento]['not_null'].min():.0f}/{st.session_state.df_dict[evento]['not_null_perc'].min():.0f}%",
+    )
+    container.metric(
+        label="Máximo de Mediciones",
+        value=f"{st.session_state.df_dict[evento]['not_null'].max():.0f}/{st.session_state.df_dict[evento]['not_null_perc'].max():.0f}%",
     )
     return container
-
-
-with col1:
-    summary_data("ESTABLE")
-with col2:
-    summary_data("INCENDIO")
-with col3:
-    summary_data("SEQUIA")
-with col4:
-    summary_data("TALA")
-with col5:
-    summary_data("VARIOS")
 
 
 col1, col2 = st.columns(2)
@@ -55,7 +64,21 @@ with col1:
 with col2:
     plot1 = (
         df.loc["Columnas"]
-        .plot(kind="bar", title="Número de Columnas", color="red", edgecolor="black")
+        .plot(
+            kind="bar",
+            title="Número de Columnas",
+            color="red",
+            edgecolor="black",
+        )
         .figure
     )
     st.pyplot(plot1, clear_figure=True)
+
+
+col1, col2, col3, col4, col5 = st.columns(5)
+cols = [col1, col2, col3, col4, col5]
+colors = ["green", "orange", "yellow", "brown", "magenta"]
+for col, evento, color in zip(cols, st.session_state.df_dict.keys(), colors):
+    with col:
+        summary_data(evento)
+        create_histogram(evento, color)
